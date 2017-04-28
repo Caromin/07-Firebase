7a1306f52ab18b76d9aae1aa73032338ddf2daa6
Caromin <c.aromin.unf@gmail.com>
2017-04-28 06:43:39 -0400

    basic outline copied over and old js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,123 +1,124 @@
-// global variables
-var random = [ "twice", "aoa", "gfriend", "pristin", "running man", "knowing bros"]
-var losses = 0;
-var wins = 0;
-var livesRemaining = 0;
-var word = "";
-var answer = [];
-var underScoreArray = [];
-var guessesArray = [];
+// Initialize personal firebase project
+  var config = {
+    apiKey: "AIzaSyBQe_qHJlfkvtfjrYTw__FxfxNdd6J7TNE",
+    authDomain: "fir-trainassignment.firebaseapp.com",
+    databaseURL: "https://fir-trainassignment.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "226263735211"
+  };
+  firebase.initializeApp(config);
 
-function pickRandom() {
-  word = random[Math.floor(Math.random() * random.length)];
-  return word;
-}
+//variables to be changed and pushed to storage and display
+var trainName = "";
+var destination = "";
+var firstTime = "";
+var frequency = 0;
 
-// word is the provided perameter from the pickRandom()
-function generateUnderscore(word) {
-// spliting word into individual characters in the underScoreArray
-	underScoreArray = word.split('');
-// setting the split word to var answer also, for comparison later on	
-	answer = word.split('');
-// creating a for loop to replace each index point with an underscore	
-	for (i = 0; i < underScoreArray.length; i++) {
-		underScoreArray.splice(i, 1, "_");
-	}
-// once the array is spliced I join the arrays with " " instead of ,'s
-	// underScoreArray = underScoreArray.join(" ");
-//console.logging to confirm changes	
-	console.log(underScoreArray);
-// messing around with javascript to identify the Id	
-	document.getElementById("hangmanWord").innerHTML= underScoreArray.join(" ");
-// adding classes to the Id section
-	$("#hangmanWord").addClass("text-center h2");
-		return underScoreArray;
-	}
+//variables for connections to firebase
+var database = firebase.database();
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected");
 
-// checks the keypress and updates the info if the letter exists
-function replacingDisplay(which) {	
-	var check = false;
-// set CharCode to variable AND also making it lowercase automatically 	
-	var event = String.fromCharCode(which).toLowerCase();
-	for (i = 0; i <answer.length; i++) {
-		if (event === answer[i]) {
-			console.log("yes it matches!");
-			underScoreArray.splice(i, 1, event);
-			document.getElementById("hangmanWord").innerHTML= underScoreArray.join(" ");
-// if there is a match in the array, then check becomes true			
-		check = true;
-		} 
-	}
-// if check is TRUE then this will run	
-	if (check) {
-// comparing the answer.join with the updating underScoreArray.join		
-		if (answer.join(" ") === underScoreArray.join(" ")) {
-			wins++;
-			$('#totalWins').html(wins);
-			alert("You won! The answer was " + word + "!");
-			$('#hangmanWord').addClass("text-success");
-			thanksForPlay();
-// setting it equal to 0 so that the keypress below will not work anymore			
-			livesRemaining = 0;
-		}
-// if check is still FALSE, then this will run
-	} else if (!check) {
-// checks the guessesArray for the event variable, if it is not here then the result is -1 === -1		
-		if ( $.inArray(event, guessesArray) === -1) {
-			livesRemaining--;
-			guessesArray.push(event);
-			$('#answerDiv').html(guessesArray.join(", "));
-			$('#answerDiv').addClass("text-danger h3");
-			$('#livesLeft').html(livesRemaining);
-// if they have no livesRemaining this will run			
-			if (livesRemaining === 0 ) {
-				alert("Game over! answer was " + word);
-				$('#hangmanWord').html(answer.join(" "));
-				$('#hangmanWord').addClass("text-danger");
-				losses++;
-				$('#totalLosses').html(losses);
-				thanksForPlay();
-		}
-		}
-	}
-}
-
-
-// restarts the game and updates the display
-function RestartGame() {	
-	pickRandom();
-	generateUnderscore(word);
-	livesRemaining = 10;
-	$('#livesLeft').html(livesRemaining);
-	$('#answerDiv').html("");
-	guessesArray = [];
-
-	$("#hangmanWord").removeClass("text-danger text-success");
-	console.log(answer);
-}
-
-// button to start game and hide the button
-$('#startBtn').on('click', function() {
-	$('#startBtn').hide();
-	RestartGame();
-})
-
-// restarts the game with lives refreshed, but total wins/losses remaining
-$('#restartGame').on('click', function() {
-	RestartGame();
-});
-
-// this runs the game key press and when there is no lives remainging, the keypress function will stop
-$(window).keypress(function(event){
-	if (livesRemaining === 0) {
-		return false;
-	} else {
-// this is the function that will check and update the display		
-	replacingDisplay(event.which); 
+//used to check if anyone is online
+connectedRef.on("value", function(snap) {
+	if(snap.val()) {
+//when someone is online they are pushed to the connects list		
+		var con = connectionsRef.push(true);
+//when someone leaves, they are removed from the list		
+		con.onDisconnect().remove();
 	}
 });
 
-function thanksForPlay() {
-	$("#hangmanImg").html("</p>Thanks for playing my demo, please check out my other works on the links above.</p>");
-	$("#hangmanImg").addClass("h2");
+//displays the number of active users
+connectionsRef.on("value", function(snap) {
+	$("#activeUsers").text("There are currently: " + snap.numChildren() + " active users");
+});
+
+//waiting for the onclick of the submit button
+ function storeInfo() {
+
+ $("#submitButton").on("click", function(event) {
+//prevents the page from refreshing 
+      event.preventDefault();
+
+//set variables to pull the values in the input value
+      trainName = $("#trainName").val().trim();
+      destination = $("#destination").val().trim();
+      firstTime = $("#firstTime").val().trim();
+      frequency = $("#frequency").val().trim();
+
+//check to see if the values are getting pulled, they are
+      console.log(trainName);
+      console.log(destination);
+
+//------------------------------------moment.js playground
+// var currentTime = moment();
+
+//     // Assumptions
+//     var tFrequency = frequency;
+
+//     // Time is 3:30 AM
+//     var first = firstTime;
+
+//     // First Time (pushed back 1 year to make sure it comes before current time)
+//     var firstTimeConverted = moment(first, "hh:mm").subtract(1, "years");
+  
+
+//         // Difference between the times
+//     var diffTime = moment().diff(firstTimeConverted, "minutes");
+
+//         // Time apart (remainder)
+//     var tRemainder = diffTime % tFrequency;
+
+//         // Minute Until Train
+//     var tMinutesTillTrain = tFrequency - tRemainder;
+
+//     // Next Train
+//     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+//----------------------------------------------
+
+//references the database and pushes the name and key to it
+database.ref("trains").push({
+
+        trainName: trainName,
+        destination: destination,
+        firstTime: firstTime,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+
+//resets the inputs, so previous inputs are not displayed
+  $('#trainName').val("");
+  $('#destination').val("");  
+  $('#firstTime').val("");
+  $('#frequency').val(0);
+    });
+
+//ends the onclick function
+};
+
+
+//function to pull previous trains from firebase database
+function displayPrevious() {
+
+database.ref("trains").orderByChild("dateAdded").on("child_added", function(snapshot) {
+
+      // Change the HTML to reflect
+      $("#trainDisplay").append('<div class="display">' + snapshot.val().trainName + '<br /></div>');
+      $("#destinationDisplay").append('<div class="display">' + snapshot.val().destination + '<br /></div>');
+      $("#firstDisplay").append('<div class="display">' + snapshot.val().firstTime + '<br /></div>');
+      $("#frequencyDisplay").append('<div class="display">' + snapshot.val().frequency + 'mins<br /></div>' );
+    });
+
 }
+
+//runs the onclick function
+storeInfo();
+
+//lets the firebase database load once at window onload to grab previous information
+$(window).on("load", function() {
+displayPrevious();
+
+
+});
